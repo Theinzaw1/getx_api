796182159fd934ca63191ebaa745c846bfb48f76
theinzaw01 <theinzarmyint@securelinkmm.com>
2023-06-15 10:12:45 +0630

    mywidget

--- lib/view/screens/home_scrren.dart
+++ lib/view/screens/home_scrren.dart
@@ -18,3 +18,17 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
